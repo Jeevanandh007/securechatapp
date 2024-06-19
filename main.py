45ee8e23dfa9da939e308d4cf0e340d8a356bd89
Jeevanandh <jeevavijay24@gmail.com>
2024-06-19 22:41:46 +0100

    basic chat room

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template, redirect, url_for, session
 from flask_socketio import SocketIO, join_room, leave_room, send
-
+import random
+from string import ascii_letters
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "supersecretkey"
 socketio = SocketIO(app)
@@ -47,8 +48,64 @@ def home():
         return redirect(url_for('room'))
     else:
         return render_template('home.html')
+    
+@app.route('/room')
+def room():
+    room = session.get('room')
+    name = session.get('name')
+    if name is None or room is None or room not in rooms:
+        return redirect(url_for('home'))
+    messages = rooms[room]['messages']
+    return render_template('room.html', room=room, user=name, messages=messages)
 
 # TODO: Build the SocketIO event handlers
+...
+@socketio.on('connect')
+def handle_connect():
+    name = session.get('name')
+    room = session.get('room')
+    if name is None or room is None:
+        return
+    if room not in rooms:
+        leave_room(room)
+    join_room(room)
+    send({
+        "sender": "",
+        "message": f"{name} has entered the chat"
+    }, to=room)
+    rooms[room]["members"] += 1
+...
+
+...
+@socketio.on('message')
+def handle_message(payload):
+    room = session.get('room')
+    name = session.get('name')
+    if room not in rooms:
+        return
+    message = {
+        "sender": name,
+        "message": payload["message"]
+    }
+    send(message, to=room)
+    rooms[room]["messages"].append(message)
+...
+
+...
+@socketio.on('disconnect')
+def handle_disconnect():
+    room = session.get("room")
+    name = session.get("name")
+    leave_room(room)
+    if room in rooms:
+        rooms[room]["members"] -= 1
+        if rooms[room]["members"] <= 0:
+            del rooms[room]
+        send({
+        "message": f"{name} has left the chat",
+        "sender": ""
+    }, to=room)
+...
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
