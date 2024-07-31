c9b94e9a636f21fc45570efb1a1dcc3d72570e17
bhuvan134 <bhuvanvignesh363@gmail.com>
2024-07-31 21:53:05 +0100

    key generation update

--- main.py
+++ main.py
@@ -49,7 +49,8 @@ def generate_room_code(length: int, existing_codes: list[str]) -> str:
 def generate_key_pair():
     private_key=
     rsa.generate_private_key(public_exponent=65537,key_size=2048)
-
+public_key = private_key.public_key()
+private_pem = private_key.private_bytes(encoding=serialization.Encoding.PEM,format=serialization.PrivateFormat.PKCS8,encryption_algorithm=serialization.NoEncryption())
 
 
 
