3daebd7a210df224a7dc2f17d9edb0eae54e19ce
bhuvan134 <bhuvanvignesh363@gmail.com>
2024-07-31 21:58:44 +0100

    key generate update

--- main.py
+++ main.py
@@ -51,7 +51,8 @@ def generate_key_pair():
     rsa.generate_private_key(public_exponent=65537,key_size=2048)
 public_key = private_key.public_key()
 private_pem = private_key.private_bytes(encoding=serialization.Encoding.PEM,format=serialization.PrivateFormat.PKCS8,encryption_algorithm=serialization.NoEncryption())
-
+public_pem = public_key.public_bytes(encoding=serialization.Encoding.PEMformat=serialization.PublicFormat.SubjectPublicKeyInfo)
+return private_pem.decode('utf-8'),public_pem.decode('utf-8')
 
 
 @app.route('/')
