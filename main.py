03dfa5ef4136c99f10055475dcdb373363c526c8
Jeevanandh <jeevavijay24@gmail.com>
2024-08-01 01:46:07 +0100

    key pair gen modification

--- main.py
+++ main.py
@@ -47,12 +47,22 @@ def generate_room_code(length: int, existing_codes: list[str]) -> str:
             return code
 
 def generate_key_pair():
-    private_key=
-    rsa.generate_private_key(public_exponent=65537,key_size=2048)
-public_key = private_key.public_key()
-private_pem = private_key.private_bytes(encoding=serialization.Encoding.PEM,format=serialization.PrivateFormat.PKCS8,encryption_algorithm=serialization.NoEncryption())
-public_pem = public_key.public_bytes(encoding=serialization.Encoding.PEMformat=serialization.PublicFormat.SubjectPublicKeyInfo)
-return private_pem.decode('utf-8'),public_pem.decode('utf-8')
+    private_key= rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048
+        )
+    public_key = private_key.public_key()
+    
+    private_pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+        )
+    public_pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo)
+    
+    return private_pem.decode('utf-8'),public_pem.decode('utf-8')
 
 
 @app.route('/')
